04c0cb687ce8c13ff03b322699d2fbebb70215cc
vanillovin <vanillovin@gmail.com>
2022-06-25 22:44:48 +0900

    220625

---
+++ inflearn/자료구조(스택, 큐)/괄호문자제거.js
@@ -0,0 +1,34 @@
+function solution(str) {
+  let answer = '';
+  let stack = [];
+
+  for (let s of str) {
+    if (s === '(') stack.push(s);
+    else if (s === ')') stack.pop();
+    else {
+      if (stack.length === 0) answer += s;
+    }
+  }
+
+  return answer;
+}
+
+function solution1(s) {
+  let answer;
+  let stack = [];
+
+  for (let x of s) {
+    if (x === ')') {
+      while (stack.pop() !== '(');
+    } else {
+      stack.push(x);
+    }
+  }
+
+  answer = stack.join('');
+  return answer;
+}
+
+let str = '(A(BC)D)EF(G(H)(IJ)K)LM(N)';
+console.log(solution(str));
+console.log(solution1(str));
---
+++ inflearn/재귀함수와 완전탐색(DFS 깊이우선탐색)/이진수_출력(재귀).js
@@ -0,0 +1,20 @@
+// * 재귀 문제는 stack frame 그리면서 풀어보기 *
+function solution(n) {
+  let answer = '';
+
+  function DFS(n) {
+    // 재귀 초보자 if-else
+    // 재귀가 언제 멈춰야 되는지. 재귀의 종착점
+    if (n === 0) return;
+    else {
+      DFS(parseInt(n / 2));
+      // console.log(n % 2);
+      answer += String(n % 2);
+    }
+  }
+  DFS(n);
+
+  return answer;
+}
+
+console.log(solution(11));
