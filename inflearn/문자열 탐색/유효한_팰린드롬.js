786efa8ad402ead14cff8c89e001eed755492824
vanillovin <vanillovin@gmail.com>
2022-09-27 23:32:39 +0900

    220927 회문문자열, 유효한팰린드롬

--- inflearn/문자열 탐색/유효한_팰린드롬.js
+++ inflearn/문자열 탐색/유효한_팰린드롬.js
@@ -1,4 +1,4 @@
-// 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않음
+// 회문을 검사할 때 순수하게 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않음
 // 알파벳 이외의 문자들을 무시합니다. 해당 문자열이 팰린드롬이면 'YES' 아니면 'NO'를 출력
 
 function solution(str) {
@@ -8,5 +8,14 @@ function solution(str) {
   return answer;
 }
 
-const str = 'found7, time: study; Yduts; emit, 7Dnouof';
-console.log(solution(str)); // 'NO'
+function solution1(s) {
+  let answer = 'YES';
+  s = s.toLowerCase();
+  s = s.split('').filter((value) => value >= 'a' && value <= 'z');
+  if (s.join('') !== s.reverse().join('')) return 'NO';
+  return answer;
+}
+
+const str = 'found7, time: study; Yduts; emit, 7Dnuof';
+console.log(solution(str)); // 'YES'
+console.log(solution1(str)); // 'YES'
--- inflearn/문자열 탐색/회문문자열.js
+++ inflearn/문자열 탐색/회문문자열.js
@@ -1,12 +1,40 @@
 // 대소문자를구분하지않음
 function solution(str) {
-  let result;
-
   return str.toLowerCase().split('').join() ==
     str.toLowerCase().split('').reverse().join()
     ? 'YES'
     : 'NO';
 }
 
+function solution1(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() !== str[str.length - i - 1].toLowerCase()) {
+      return 'NO';
+    } else {
+      return 'YES';
+    }
+  }
+}
+
+// string.length / 2 까지만 돌기
+function solution2(str) {
+  let answer = 'YES';
+  str = str.toLowerCase();
+  let len = str.length;
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (str[i] !== str[len - i - 1]) return 'NO';
+  }
+  return answer;
+}
+
 console.log(solution('gooG')); // YES
+console.log(solution1('gooG')); // YES
+console.log(solution2('gooG')); // YES
+
+console.log(solution('goooG')); // YES
+console.log(solution1('goooG')); // YES
+console.log(solution2('goooG')); // YES
+
 console.log(solution('hello')); // NO
+console.log(solution1('hello')); // NO
+console.log(solution2('hello')); // NO
