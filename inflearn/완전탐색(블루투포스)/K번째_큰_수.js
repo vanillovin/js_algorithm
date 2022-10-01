992a4b91fb8ad2523064bb65977460a3af6671d9
vanillovin <vanillovin@gmail.com>
2022-10-01 19:11:18 +0900

    K번째 큰 수

---
+++ inflearn/완전탐색(블루투포스)/K번째_큰_수.js
@@ -0,0 +1,21 @@
+function solution(input = '10 3\n13 15 34 23 45 65 33 11 26 42') {
+  const [_, nums] = input.split('\n');
+  const [N, K] = _.split(' ');
+  const arr = nums.split(' ').map(Number); // ❓
+  let set = new Set();
+
+  for (let i = 0; i < N; i++) {
+    for (let j = i + 1; j < N; j++) {
+      for (let k = j + 1; k < N; k++) {
+        set.add(arr[i] + arr[j] + arr[k]);
+      }
+    }
+  }
+
+  // Array.from() 함수는 유사배열객체나 반복가능객체를 얕은 복사하여 새로운 배열을 만듦
+  // 유사배열객체(array-like object) : length 속성과 index element를 가지는 객체
+  // 반복가능객체(iterable object) : 배열을 일반화한 객체 ex)Map, Set
+  return Array.from(set).sort((a, b) => b - a)[K - 1];
+}
+
+console.log(solution()); // 143
