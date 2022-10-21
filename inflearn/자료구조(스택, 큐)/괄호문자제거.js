67f8b509c1b4000ff92aa95277be05e238726e12
vanillovin <vanillovin@gmail.com>
2022-10-21 22:15:52 +0900

    221021

--- inflearn/자료구조(스택, 큐)/괄호문자제거.js
+++ inflearn/자료구조(스택, 큐)/괄호문자제거.js
@@ -1,7 +1,6 @@
 function solution(str) {
   let answer = '';
   let stack = [];
-
   for (let s of str) {
     if (s === '(') stack.push(s);
     else if (s === ')') stack.pop();
@@ -9,14 +8,12 @@ function solution(str) {
       if (stack.length === 0) answer += s;
     }
   }
-
   return answer;
 }
 
 function solution1(s) {
   let answer;
   let stack = [];
-
   for (let x of s) {
     if (x === ')') {
       while (stack.pop() !== '(');
@@ -24,11 +21,10 @@ function solution1(s) {
       stack.push(x);
     }
   }
-
   answer = stack.join('');
   return answer;
 }
 
 let str = '(A(BC)D)EF(G(H)(IJ)K)LM(N)';
-console.log(solution(str));
-console.log(solution1(str));
+console.log(solution(str)); // EFLM
+console.log(solution1(str)); // EFLM
--- inflearn/효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/연속_부분수열_2.js
+++ inflearn/효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/연속_부분수열_2.js
@@ -1,26 +1,24 @@
-// 뺴놓고도카운팅
-function solution(m, arr) {
-  let answer = 0,
-    lt = 0,
-    sum = 0;
-
+// rt가 증가하면서 더하는 것도 카운팅, lt도 빼놓고도 카운팅
+// rt가 증가해서 "새로운 숫자가 추가돼야지" 새로운 숫자가 연속부분수열의 마지막이면서 개수가 구해짐.
+function solution(M, arr) {
+  let answer = 0;
+  let lt = 0;
+  let sum = 0;
   for (let rt = 0; rt < arr.length; rt++) {
     sum += arr[rt];
-    while (sum > m) {
+    while (sum > M) {
       sum -= arr[lt++];
     }
     answer += rt - lt + 1;
   }
-
   return answer;
 }
 
 function solution1(m, arr) {
-  let answer = 0,
-    lt = 0,
-    rt = 0,
-    sum = arr[rt];
-
+  let answer = 0;
+  let lt = 0;
+  let rt = 0;
+  let sum = arr[rt];
   while (rt < arr.length) {
     if (sum <= m) {
       answer += rt - lt + 1;
@@ -31,10 +29,25 @@ function solution1(m, arr) {
       lt++;
     }
   }
-
   return answer;
 }
 
-let arr = [1, 3, 1, 2, 3];
+let arr = [1, 3, 1, 2, 3]; // 5개의 연속수열로 5이하가되면 연속부분수열
 console.log(solution(5, arr)); // 10
 console.log(solution1(5, arr)); // 10
+
+// 새로운 숫자가 추가되면(ex.3) 무조건 rt가 증가해서 sum에 추가되면
+// 그 숫자가 포함된 (포함하는) 연속부분수열은 무조건 한개이상임. (5이하면)
+// 1 3 <- 이 구간에서 연속부분수열은 {1} {1, 3}
+// 1
+// 1 3
+// sum이 M 이하니까 rt가 증가하면서
+// 1 3 1(rt)
+// => sum : 5
+// 이하니까 카운팅하는데, 새롭게 추가된 1이 포함된 연속부분수열
+// {1} {1,3} {1,3,1} 이렇게 3개
+// 그럼 이 개수는 어떻게 구하나?
+// (lt)1 3 1(rt) <- 이렇게 3개가 연속부분수열이면
+// 새로 추가된 끝숫자가 포함된 연속부분수열은
+// => rt - lt + 1 = 3
+// 합이5면카운팅 -> 또rt증가해서 더했는데 7 -> lt는빼면서증가 -> 5이하가될때까지반복더
